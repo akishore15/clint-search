5a34a79db360e73053e933e81317a56ba86fe414
hitor- 1 <154870205+akishore15@users.noreply.github.com>
2024-12-08 17:29:17 -0800

    run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,26 @@
 sudo apt install git
-sudo ufw allow 8085
-git clone https://github.com/akishore15/clint-search.git --p 8085
-echo "BOOTING ON PORT 8085"
-google-chrome localhost:8085
+echo "Do you want to install? (y/n)"
+read ans1
+if [ ans1 -eq "y" ]; then
+echo "OK"
+sudo ufw allow --p 8085
+git clone https://github.com/akishore15/clint-search
+echo "Booting on a port, choose which one."
+read pt
+git clone https://github.com/akisore15/clint-search/google.py --p $pt
+echo "*-----"
+sleep 1
+echo "**----"
+sleep 1
+echo "***---"
+sleep 1
+echo "****--"
+sleep 1
+echo "*****-"
+sleep 1
+echo "******"
+sleep 1
+echo "BOOTED! Go to localhost:$pt to view your own Google Search!"
+else
+echo "OK, deleting."
+fi
