04dab755a6a53407ad6bfd5ff03cd1c66e4f5a5d
hitor- 1 <154870205+akishore15@users.noreply.github.com>
2024-12-08 17:31:33 -0800

    run.sh

--- run.sh
+++ run.sh
@@ -3,10 +3,9 @@ echo "Do you want to install? (y/n)"
 read ans1
 if [ ans1 -eq "y" ]; then
 echo "OK"
-sudo ufw allow --p 8085
-git clone https://github.com/akishore15/clint-search
 echo "Booting on a port, choose which one."
 read pt
+sudo ufw allow --p $pt
 git clone https://github.com/akisore15/clint-search/google.py --p $pt
 echo "*-----"
 sleep 1
@@ -24,3 +23,7 @@ echo "BOOTED! Go to localhost:$pt to view your own Google Search!"
 else
 echo "OK, deleting."
 fi
+echo "Leaving portal..."
+sleep 3
+exit
+
